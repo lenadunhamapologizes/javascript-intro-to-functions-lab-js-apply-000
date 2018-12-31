933d87077fc6244135995382f5413c17766c1efc
Nancy Mitchell <mitcheaven@gmail.com>
2018-12-31 05:20:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,9 +9,13 @@ let logShout = (string) => console.log(string.toUpperCase())
 let logWhisper = (string) => console.log(string.toLowerCase())
 
 function sayHiToGrandma(string) {
+<<<<<<< HEAD
   if (string === string.toLowerCase()) return 'I can\'t hear you!'
   
   if (string === string.toUpperCase()) return 'YES INDEED!'
+=======
+  if (string === string.toLowerCase()) return 
+>>>>>>> 09e2f773367577c3bbe52e810ceaf97e31fa1f45
   
   if (string === 'I love you, Grandma.') return 'I love you, too.'
 }
